a16834cbd2cd327095188c1f6defd91b9006dad2
Medj41 <larbi.djouama@gmail.com>
2024-01-30 22:30:32 +0000

    fixed location list

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,27 @@
+let locationAdd;
 let searchBtn = $('#search-button');
 let LocationList = $('.list-group');
 let currentWeather = $('#today');
+
 var today = dayjs();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 
 const locations = [];
+
 searchBtn.on('click',  addlocation);
 
  // Add location to Locations array
-function addlocation(){
+function addlocation(event){
   let inputElement = $('#search-input');
-  let locationAdd = inputElement.val();
+  locationAdd = inputElement.val();
   locations.push(locationAdd);
   console.log(locations);
   console.log(locationAdd)
   renderLocationlist();
   inputElement.val('');
   
-  return false;
+  event.preventDefault();
+  
 }
 
 //loop throughthe array and display each location 
@@ -25,9 +29,12 @@ function addlocation(){
 
 function renderLocationlist(){
 
-  // var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+ locationAdd + "&appid=9740464f964306bad708a660e0c862fe";
-  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=london&appid=9740464f964306bad708a660e0c862fe";
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+ locationAdd + "&appid=9740464f964306bad708a660e0c862fe";
 
+  console.log(locationAdd);
+  
+currentWeather.empty();
+LocationList.empty();
   // Creates a Fetch call for the specific movie button being clicked
   fetch(queryURL)
     .then(function (response) {
